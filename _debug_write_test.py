730ab858f0e5f2bce20711dbbc97820b6e32f924
Sergio Garcia <47191307+sgarciapuga@users.noreply.github.com>
2026-08-09 09:40:45 +0100

    removed temp files

--- _debug_write_test.py
+++
@@ -1,5 +0,0 @@
-import os
-print('cwd', os.getcwd())
-with open('debug_write_test.txt','w', encoding='utf-8') as f:
-    f.write('hello')
-print('wrote file, exists?', os.path.exists('debug_write_test.txt'))
--- _env_debug.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-from pathlib import Path
-print('sys.executable', sys.executable)
-print('sys.prefix', sys.prefix)
-print('CONDA_PREFIX', os.environ.get('CONDA_PREFIX'))
-print('CONDA_DEFAULT_ENV', os.environ.get('CONDA_DEFAULT_ENV'))
-print('\nFull PATH entries:')
-for p in os.environ.get('PATH','').split(os.pathsep):
-    print(p)
